cf9b4a6c0f91a0aea3e624f91690398608e00b21
ameliorate <ameilorate2@gmail.com>
2015-08-05 14:25:30 -0400

    Fixes core to actually do auth stuff and things
    I accidentally did the wrong thing when copying files.

--- src/reddit_clj_that_works/internal/core.clj
+++ src/reddit_clj_that_works/internal/core.clj
@@ -1,28 +1,22 @@
-(ns reddit-clj-that-works.reddit
-  (:require [org.httpkit.client :as http]
-            [reddit-clj-that-works.internal.core :as core]))
+(ns reddit-clj-that-works.internal.core
+  "Core functionality for reddit api wrapper"
+  (:require [cheshire.core :as json]
+            [reddit-clj-that-works.internal.util :as util]))
 
-;; --- Auth and login --
+;; --- Auth and login ---
 
-(defn login
-  "Log in to a reddit account and initalise certan network functionality."
-  [username password client-id client-secret user-agent]
-  (let [options-unauth {:user-agent user-agent}]
-    (let [auth (core/auto-auth username password client-id client-secret options-unauth)]
-      {:username username
-       :password password
-       :client-id client-id
-       :client-seceret client-secret
-       :user-agent user-agent
-       :auth auth})))
+(defn auth
+  "Creates an auth object"
+  [username password client-id client-secret options]
+  (json/parse-string (http/post (conj options {:url "https://www.reddit.com/api/v1/access_token"
+                                               :query-params {"grant_type=password" (str "username=" username) (str "password=" password)}
+                                               :basic-auth [client-id client-secret]})) true))
 
-(defn user-agent
-  "Forms a user agent in the reddit reccomended way."
-  [platform app-id version creator-username]
-  (str platform ":" app-id ":" version " (by /u/" creator-username ")"))(ns reddit-clj-that-works.internal.core
-  "Core functionality for reddit api wrapper")
-
-(defmacro reddit
-  "For internal use in generating urls."
-  [& rest]
-  (str "http://www.reddit.com/" (clojure.string/join "/" rest)))
+(defn auto-auth
+  "Automatically refreshes an auth atom every 50 minutes."
+  [username password client-id client-secret options]
+  (let [auth (atom (auth username password client-id client-secret options))]
+    (util/forever-thread
+      (Thread/sleep (* 1000 (* 60 50)))
+      (swap! auth (fn [_] (auth username password client-id client-secret options))))
+    auth))
